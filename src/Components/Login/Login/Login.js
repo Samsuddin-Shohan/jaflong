73e2c3d69b092a49aa64d6e73eb15a9c0d5789e2
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-27 02:08:05 +0600

    login and registration page created

--- src/Components/Login/Login/Login.js
+++ src/Components/Login/Login/Login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './Login.css';
+import banner from '../../../assets/bannerbackground.png'
+import NavBar from '../../NavBar/NavBar';
+import Footer from '../../Footer/Footer';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo2.png';
 
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
+        <NavBar></NavBar>
+        <div id='login'>
+        <div className=' d-flex flex-column  align-items-center' id='login-form'>
+            <img id='main-logo-login' src={logo} alt="" />
+           
+            <input id = 'form-element' type="text" name=""  placeholder='Email'/>
+            <input id = 'form-element' type="text" name="" placeholder='Password'/>
+            
+            <button id = 'form-element'  className='btn btn-secondary'>Log In</button>
+            <p className='fs-2'>or</p>
+            <Link to={`/login`}>
+                 <button id='login-link'  className='btn btn-outline-secondary '>Google Sign In</button>
+            </Link>
+
+        </div>
+        </div>
+        <Footer></Footer>
+
+           
         </div>
     );
 };
--- src/Components/Login/Registration/Registration.js
+++ src/Components/Login/Registration/Registration.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import NavBar from '../../NavBar/NavBar';
+import logo from '../../../assets/logo2.png';
+import './Registration.css';
+import { Link } from 'react-router-dom';
+import Footer from '../../Footer/Footer';
 const Registration = () => {
     return (
         <div>
-            <h1>Registration</h1>
+        <NavBar></NavBar>
+        <div id='login'>
+        <div className=' d-flex flex-column  align-items-center' id='login-form'>
+            <img id='main-logo-login' src={logo} alt="" />
+            <input id = 'form-element' type="text" name="" placeholder='Name'/>
+            <input id = 'form-element' type="text" name=""  placeholder='Email'/>
+            <input id = 'form-element' type="text" name="" placeholder='Password'/>
+            <input id = 'form-element' type="text" name=""placeholder='Confirm Password'/>
+            <button id = 'form-element'  className='btn btn-secondary'>Sign Up</button>
+            <p className='fs-2'>or</p>
+            <Link to={`/login`}>
+                 <button id='login-link'  className='btn btn-outline-secondary '>Already have an account?</button>
+            </Link>
+
+        </div>
+        </div>
+        <Footer></Footer>
+
+           
         </div>
     );
 };
