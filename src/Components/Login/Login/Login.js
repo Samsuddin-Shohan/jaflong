031f366f7741ef237fb1f39270bc49cb5a669f35
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-27 02:46:53 +0600

    state updated of login and registration page

--- src/Components/Login/Login/Login.js
+++ src/Components/Login/Login/Login.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import banner from '../../../assets/bannerbackground.png'
 import NavBar from '../../NavBar/NavBar';
 import Footer from '../../Footer/Footer';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo2.png';
+import { Form } from 'react-bootstrap';
 
 const Login = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const handleLogin = (event)=>{
+        event.preventDefault();
+        console.log(email);
+        setEmail('');
+        setPassword('');
+    }
     return (
         <div>
         <NavBar></NavBar>
         <div id='login'>
-        <div className=' d-flex flex-column  align-items-center' id='login-form'>
+        <Form className=' d-flex flex-column  align-items-center' id='login-form'>
             <img id='main-logo-login' src={logo} alt="" />
            
-            <input id = 'form-element' type="text" name=""  placeholder='Email'/>
-            <input id = 'form-element' type="text" name="" placeholder='Password'/>
+            <input value={email} onChange={event => setEmail(event.target.value)} id = 'form-element' type="text" name=""  placeholder='Email'/>
+            <input value={password} onChange={event => setPassword(event.target.value)} id = 'form-element' type="password" name="" placeholder='Password'/>
             
-            <button id = 'form-element'  className='btn btn-secondary'>Log In</button>
+            <button id = 'form-element'  className='btn btn-secondary' onClick={handleLogin}>Log In</button>
             <p className='fs-2'>or</p>
             <Link to={`/login`}>
                  <button id='login-link'  className='btn btn-outline-secondary '>Google Sign In</button>
             </Link>
 
-        </div>
+        </Form>
         </div>
         <Footer></Footer>
 
--- src/Components/Login/Registration/Registration.js
+++ src/Components/Login/Registration/Registration.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../../NavBar/NavBar';
 import logo from '../../../assets/logo2.png';
 import './Registration.css';
 import { Link } from 'react-router-dom';
 import Footer from '../../Footer/Footer';
+import { Form } from 'react-bootstrap';
 const Registration = () => {
+    const [name,setName]=useState('');
+    const [pasword,setPassword] = useState('');
+    const [confirmPasword,setConfirmPassword] = useState('');
+    const [email,setEmail] = useState('');
+    const handleSignUp = (event)=>{
+        event.preventDefault();
+        console.log(name + ':'+ email);
+        setName('');
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+        
+        
+    }
     return (
         <div>
         <NavBar></NavBar>
         <div id='login'>
-        <div className=' d-flex flex-column  align-items-center' id='login-form'>
+        <Form className=' d-flex flex-column  align-items-center' id='login-form'>
+            
             <img id='main-logo-login' src={logo} alt="" />
-            <input id = 'form-element' type="text" name="" placeholder='Name'/>
-            <input id = 'form-element' type="text" name=""  placeholder='Email'/>
-            <input id = 'form-element' type="text" name="" placeholder='Password'/>
-            <input id = 'form-element' type="text" name=""placeholder='Confirm Password'/>
-            <button id = 'form-element'  className='btn btn-secondary'>Sign Up</button>
+            <input value={name} onChange={event => setName(event.target.value)}  id = 'form-element' type="text" name="" placeholder='Name'/>
+            <input  value={email} onChange={event => setEmail(event.target.value)} id = 'form-element' type="text" name=""  placeholder='Email'/>
+            <input  value={pasword} onChange={event => setPassword(event.target.value)} id = 'form-element' type="password" name="" placeholder='Password'/>
+            <input  value={confirmPasword} onChange={event => setConfirmPassword(event.target.value)} id = 'form-element' type="password" name=""placeholder='Confirm Password'/>
+            <button id = 'form-element'  className='btn btn-secondary' onClick={handleSignUp}>Sign Up</button>
             <p className='fs-2'>or</p>
             <Link to={`/login`}>
                  <button id='login-link'  className='btn btn-outline-secondary '>Already have an account?</button>
             </Link>
 
-        </div>
+        </Form>
         </div>
         <Footer></Footer>
 
